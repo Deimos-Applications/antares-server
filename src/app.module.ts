509044ef4c746e67290cabbb5a3d03bc34e99f99
Brandon G. Neri <arcaniteamp@gmail.com>
2021-05-12 20:02:03 -0500

    feat(SERVER-3): Add minimal i18n support

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,24 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { I18nJsonParser, I18nModule, QueryResolver } from 'nestjs-i18n';
+import * as path from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot(),
-    UserModule,
     AuthModule,
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      parser: I18nJsonParser,
+      parserOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      resolvers: [{ use: QueryResolver, options: ['lang'] }],
+    }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
